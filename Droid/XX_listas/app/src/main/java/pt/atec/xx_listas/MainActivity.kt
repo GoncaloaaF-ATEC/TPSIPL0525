ac429a495dfbd4c6178162f459e1e3cca350157f
GoncaloaaF <goncaloaaf@gmail.com>
2026-01-29 16:56:56 +0000

    demo listas git push

---
+++ Droid/XX_listas/app/src/main/java/pt/atec/xx_listas/MainActivity.kt
@@ -0,0 +1,63 @@
+package pt.atec.xx_listas
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.unit.sp
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Lista(myList = listOf("Gonçalo", "João", "Ana", "Maria"),
+            onClick = { arg1, arg2 ->
+
+                    Log.d(arg1, "o nome tem $arg2 letras")
+            })
+        } // setContent
+    } // onCreate
+}
+
+
+@Composable
+fun Lista(myList: List<String>, onClick: (String, Int) -> Unit ) {
+
+    LazyColumn(modifier = Modifier.padding(vertical = 15.dp)){
+        items(myList) { nome ->
+
+            Text(nome, modifier = Modifier
+                .padding(20.dp)
+                .fillMaxSize()
+                .clickable{
+                    onClick(nome,nome.length)
+                },
+                style = MaterialTheme.typography.bodyLarge
+
+            )
+        }
+
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+
+}
